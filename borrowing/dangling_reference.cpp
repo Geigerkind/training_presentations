8bf16de4ad381e12198f84534d0f383cb23393ed
Tom Dymel <td@wps.de>
2020-02-12 15:38:43 +0100

    Dangling sauberer

--- borrowing/dangling_reference.cpp
+++ borrowing/dangling_reference.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 
+int* dangling() {
+    int *a = (int*)malloc(sizeof(int));
+    *a = 2*3*7;
+    delete a;
+    return a;
+}
+
 int main() {
-    int *answer;
-    {
-        int calc_answer = 2*3*7;
-        answer = &calc_answer;
-    }
+    int *answer = dangling();
     std::cout << "Address of answer: " << answer << std::endl;
     std::cout << "Answer is: " << *answer << std::endl;
 }
